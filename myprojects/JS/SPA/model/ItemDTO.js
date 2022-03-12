65ea2164cb2cbfb3614a0939c39cf57084c032c2
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-12 17:18:04 +0530

    update ItemDTO.js in spa

--- myprojects/JS/SPA/model/ItemDTO.js
+++ myprojects/JS/SPA/model/ItemDTO.js
@@ -1,7 +1,56 @@
-function ItemDTO(code,name,price,quantity) {
-    var __code=code;
-    var __name=name;
-    var __price=price;
-    var __quantity=quantity;
+function ItemDTO(icode,iname,iprice,iqty) {
 
+    var code =icode;
+    var name=iname;
+    var price=iprice;
+    var qty=iqty;
+
+
+    Object.defineProperty(this,"icode",{
+        get:function()
+        {
+            return code;
+        },
+        set:function(icode)
+        {
+            this.code=icode;
+        }
+    });
+
+
+    Object.defineProperty(this,"iname",{
+        get:function()
+        {
+            return name;
+        },
+        set:function(iname)
+        {
+            this.name=iname;
+        }
+    });
+
+
+    Object.defineProperty(this,"iprice",{
+        get:function()
+        {
+            return price;
+        },
+        set:function(iprice)
+        {
+            this.price=iprice;
+        }
+    });
+
+
+    Object.defineProperty(this,"iqty",{
+        get:function()
+        {
+            return qty;
+        },
+        set:function(iqty)
+        {
+            this.qty=iqty;
+        }
+    });
 }
+
