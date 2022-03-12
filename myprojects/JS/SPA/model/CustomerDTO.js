9da6206d5be067590acb355f3eb0115605427a09
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-12 17:17:54 +0530

    update CustomerDTO.js in spa

--- myprojects/JS/SPA/model/CustomerDTO.js
+++ myprojects/JS/SPA/model/CustomerDTO.js
@@ -1,7 +1,55 @@
-function CustomerDTO(id,name,address,salary) {
-    var __id=id;
-    var __name=name;
-    var __address=address;
-    var __salary=salary;
+function CustomerDTO(cid,cname,caddress,csalary) {
 
+    var id =cid;
+    var name=cname;
+    var address=caddress;
+    var salary=csalary;
+
+
+    Object.defineProperty(this,"cid",{
+        get:function()
+        {
+            return id;
+        },
+        set:function(cid)
+        {
+            this.id=cid;
+        }
+    });
+
+
+    Object.defineProperty(this,"cname",{
+        get:function()
+        {
+            return name;
+        },
+        set:function(cname)
+        {
+            this.name=cname;
+        }
+    });
+
+
+    Object.defineProperty(this,"caddress",{
+        get:function()
+        {
+            return address;
+        },
+        set:function(caddress)
+        {
+            this.address=caddress;
+        }
+    });
+
+
+    Object.defineProperty(this,"csalary",{
+        get:function()
+        {
+            return salary;
+        },
+        set:function(csalary)
+        {
+            this.salary=csalary;
+        }
+    });
 }
